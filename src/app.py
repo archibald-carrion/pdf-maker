ae74a2b5ce13d758df22c862467616ec387500f1
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-04-25 18:12:28 -0600

    basic app was added to the project.

---
+++ src/app.py
@@ -0,0 +1,27 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+import os
+import eightPuzzleSolver
+# import time
+# import psutil
+# import matrices
+
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("green")
+
+# custom_font = ("Helvetica", 30) # font used in the buttons of the puzzle
+# empty_tile = "    " # text used in the empty tile
+          
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        # configure window
+        self.title("8 Puzzle Solver")
+        self.geometry(f"{1480}x{720}")
+
+        # configure grid layout (4x4) and others general configurations
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure(0, weight=1)

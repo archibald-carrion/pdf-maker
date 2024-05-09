aeb6022137e6d26af24da9633e034fb34aabe1ea
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-05-08 19:07:43 -0600

    add change_appearance_mode_event function to the project.

--- src/app.py
+++ src/app.py
@@ -29,6 +29,9 @@ class App(customtkinter.CTk):
         # self.init_left_sidebar()
 
 
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
